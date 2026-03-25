93bd5369fe14fc58f7baffbd1aad7328e635689e
Altitudevision <altitudevis3n@gmail.com>
2026-03-25 18:13:05 +0100

    Update altcomData

--- client/src/pages/altcom/altcomData.js
+++ client/src/pages/altcom/altcomData.js
@@ -207,8 +207,8 @@ export const SERVICES = [
         ],
         images: [
             'https://images.unsplash.com/photo-1492691527719-9d1e07e534b4?w=800&q=80', // photography event
-            'https://images.unsplash.com/photo-1598387993441-a364f854cfbd?w=800&q=80', // video production
-            'https://images.unsplash.com/photo-1540575467063-178a50c2df87?w=800&q=80', // event coverage
+            'https://images.unsplash.com/photo-1664277497086-e21115efbbf4?q=80&w=2063&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', // video production
+            'https://images.unsplash.com/photo-1647427854253-b92bb40c9330?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', // studio photo
         ],
         process: [
             {
